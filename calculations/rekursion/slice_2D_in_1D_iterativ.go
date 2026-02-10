e003a8c1da57644eecf23bd5ef2abfc256c1c5b8
StK2309 <s251903@student.dhbw-mannheim.de>
2026-02-10 18:17:51 +0100

    2D in 1D

--- calculations/rekursion/slice_2D_in_1D_iterativ.go
+++ calculations/rekursion/slice_2D_in_1D_iterativ.go
@@ -1,19 +1,35 @@
 package rekursion
 
+import "slices"
+
 // Erwartet eine verschachtelte Liste von Ganzzahlen (int).
 // Jede innere Liste kann beliebig viele Zahlen enthalten.
 // Die Funktion soll alle Zahlen zu einem einzigen Slice zusammenführen („flatten“).
 //
 // Beispiel:
-// Flatten([][]int{
-//     {1, 2},
-//     {},
-//     {3, 4, 5},
-//     {6},
-// }) → []int{1, 2, 3, 4, 5, 6}
+//
+//	Flatten([][]int{
+//	    {1, 2},
+//	    {},
+//	    {3, 4, 5},
+//	    {6},
+//	}) → []int{1, 2, 3, 4, 5, 6}
 //
 // Die Funktion soll iterativ geschrieben werden
 func FlattenIterativ(nested [][]int) []int {
 	// TODO: Funktion implementieren (rekursiv oder iterativ)
-	return nil // Platzhalterwert
+	var list []int
+
+	if len(nested) == 0 {
+		return list
+	}
+
+	for _, innerlist := range nested {
+		for _, i := range innerlist {
+			if !slices.Contains(list, i) {
+				list = append(list, i)
+			}
+		}
+	}
+	return list
 }
--- calculations/rekursion/slice_2D_in_1D_rekursiv.go
+++ calculations/rekursion/slice_2D_in_1D_rekursiv.go
@@ -5,15 +5,29 @@ package rekursion
 // Die Funktion soll alle Zahlen zu einem einzigen Slice zusammenführen („flatten“).
 //
 // Beispiel:
-// Flatten([][]int{
-//     {1, 2},
-//     {},
-//     {3, 4, 5},
-//     {6},
-// }) → []int{1, 2, 3, 4, 5, 6}
+//
+//	Flatten([][]int{
+//	    {1, 2},
+//	    {},
+//	    {3, 4, 5},
+//	    {6},
+//	}) → []int{1, 2, 3, 4, 5, 6}
 //
 // Die Funktion soll rekursiv geschrieben werden
 func FlattenRekursiv(nested [][]int) []int {
 	// TODO: Funktion implementieren (rekursiv oder iterativ)
-	return nil // Platzhalterwert
+	var list []int
+
+	if len(nested) == 0 {
+		return list
+	}
+	list = append(nested[0], FlattenRekursiv(nested[1:])...)
+
+	//for _, i := range list {
+	//	if slices.Contains(list, list[i]) {
+	//		list = slices.Delete(list, i, i)
+	//	}
+	//}
+
+	return list
 }
